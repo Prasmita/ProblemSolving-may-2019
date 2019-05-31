2928e6c304515df82bca343b5433181759627f44
Prasmitha <prasmitha.vits@gmail.com>
2019-05-31 19:48:31 +0530

    add

---
+++ problemsolving/raptor/arithmeticOperations.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? sum;
+   ?? a;
+
+   raptor_prompt_variable_zzyz =" Enter First Number ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   raptor_prompt_variable_zzyz =" Enter Second Number ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   sum =a+b;
+   cout << " Average is "+a+" and "+b+" is "+(sum/2) << endl;
+   return 0;
+}
---
+++ problemsolving/raptor/arithmeticOperations.java
@@ -0,0 +1,25 @@
+/**
+  * NAME:
+  * DATE:
+  * FILE:
+  * COMMENTS:
+  */
+
+public class arithmeticOperations extends eecs.Gui
+{
+   public static void main(String[] args)
+   {
+      // declare variables
+      String raptor_prompt_variable_zzyz = null;
+      ?? A = ??;
+      ?? sum = ??;
+      ?? B = ??;
+      
+      raptor_prompt_variable_zzyz = " Enter First Number ";
+      A = get???(raptor_prompt_variable_zzyz);
+      raptor_prompt_variable_zzyz = " Enter Second Number ";
+      B = get???(raptor_prompt_variable_zzyz);
+      sum = a + b;
+      printLine(" Average is " + a + " and " + b + " is " + (sum / 2));
+   } // close main
+} // close arithmeticOperations
